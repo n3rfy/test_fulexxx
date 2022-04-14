221018264b61540f4bd86a497735470104f34f8b
Daniil Ogloblin <daniil.oglooblin@gmail.com>
2022-04-14 21:15:54 +0300

    add test

--- tests/test_api/test_users.py
+++ tests/test_api/test_users.py
@@ -1,3 +1,6 @@
+import sys
+sys.path = ['', '..'] + sys.path[1:]
+
 import requests
 from fastapi import status
 from fastapi.testclient import TestClient
@@ -8,10 +11,9 @@ client: requests.Session = TestClient(app)
 
 anvdev_user = {
     'id': 28631927,
-    'login': 'anvdev',
+    'login': 'n3rfy',
     'name': 'Ivan Titov'
 }
-
 
 def test_user():
     response = client.put('/v1/users', json=anvdev_user)
@@ -19,6 +21,9 @@ def test_user():
 
     response = client.get('/v1/users')
     assert response.json() == [anvdev_user]
+
+    response = client.get(f'/v1/users/{anvdev_user["id"]}/stats?date_from=2022-01-13&date_to=2022-04-14')
+    assert response.status_code == status.HTTP_200_OK
 
     response = client.delete(f'/v1/users/{anvdev_user["id"]}')
     assert response.status_code == status.HTTP_200_OK
